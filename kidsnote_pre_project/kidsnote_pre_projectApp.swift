13d3a3b93eb83deca8215702a251330c4d6da1d7
NakCheon Jung <ncjung1000@gmail.com>
2024-07-31 11:15:11 +0900

    HW-1 create project with xcode.

---
+++ kidsnote_pre_project/kidsnote_pre_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  kidsnote_pre_projectApp.swift
+//  kidsnote_pre_project
+//
+//  Created by Nakcheon Jung on 7/31/24.
+//
+
+import SwiftUI
+
+@main
+struct kidsnote_pre_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
